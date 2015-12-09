65db1e829699985daac4ac3f0db06f19902dba57
Malu <gusani_fer@hotmail.com>
2015-12-09 14:12:18 -0600

    Formulario de turnos

--- routes/turnos.js
+++ routes/turnos.js
@@ -3,8 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('turnos/index', { title: 'Listado de Turnos' });
 });
-
+router.get('/registro', function(req, res, next) {
+  res.render('turnos/new', { title: 'Nuevo Turno' });
+});
 
 module.exports = router;
