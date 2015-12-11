3c50ea3d1ee4f263bd06262531e05102e3024efc
Cesariux 23 <cesariux23@gmail.com>
2015-12-11 17:54:14 -0600

    Cambio a bootstrap, plantilla AdminLTE, free version

--- routes/emision.js
+++ routes/emision.js
@@ -101,12 +101,312 @@ var documentos=[
     ],
     descripcion:"Instalación del active directoy en Dirección general",
     elaboro:"AZG/htg",
+  },
+  {
+    documento:"Circular",
+    tipo:"emitido",
+    emision:"Interno",
+    numero:"SA/57/2015",
+    fecha:"17/11/2015",
+    nivel:"Subdirección",
+    area:"SA",
+    departamento:"SA01",
+    firma:{
+      nombre:"M.P.P Aldo Zavala García",
+      cargo:"Subdirector Administrativo"
+    },
+    asunto:"INSTALACION DEL SERVICIOS DE DIRECTORIO ACTIVO",
+    destinatarios:
+    [
+      {
+        nombre:"SUBDIRECTORES DE AREA, JEFES DE UNIDAD Y JEFES DE DEPARTAMENTO",
+        cargo:"IVEA"
+      },
+      {
+        nombre:"César Encarnación Mendoza",
+        atn:true
+      }
+    ],
+    descripcion:"Instalación del active directoy en Dirección general",
+    elaboro:"AZG/htg",
+  },
+  {
+    documento:"Circular",
+    tipo:"emitido",
+    emision:"Interno",
+    numero:"SA/57/2015",
+    fecha:"17/11/2015",
+    nivel:"Subdirección",
+    area:"SA",
+    departamento:"SA01",
+    firma:{
+      nombre:"M.P.P Aldo Zavala García",
+      cargo:"Subdirector Administrativo"
+    },
+    asunto:"INSTALACION DEL SERVICIOS DE DIRECTORIO ACTIVO",
+    destinatarios:
+    [
+      {
+        nombre:"SUBDIRECTORES DE AREA, JEFES DE UNIDAD Y JEFES DE DEPARTAMENTO",
+        cargo:"IVEA"
+      }
+    ],
+    descripcion:"Instalación del active directoy en Dirección general",
+    elaboro:"AZG/htg",
+  },
+  {
+    documento:"Circular",
+    tipo:"emitido",
+    emision:"Interno",
+    numero:"SA/57/2015",
+    fecha:"17/11/2015",
+    nivel:"Subdirección",
+    area:"SA",
+    departamento:"SA01",
+    firma:{
+      nombre:"M.P.P Aldo Zavala García",
+      cargo:"Subdirector Administrativo"
+    },
+    asunto:"INSTALACION DEL SERVICIOS DE DIRECTORIO ACTIVO",
+    destinatarios:
+    [
+      {
+        nombre:"SUBDIRECTORES DE AREA, JEFES DE UNIDAD Y JEFES DE DEPARTAMENTO",
+        cargo:"IVEA"
+      }
+    ],
+    descripcion:"Instalación del active directoy en Dirección general",
+    elaboro:"AZG/htg",
+  },
+  {
+    documento:"Circular",
+    tipo:"emitido",
+    emision:"Interno",
+    numero:"SA/57/2015",
+    fecha:"17/11/2015",
+    nivel:"Subdirección",
+    area:"SA",
+    departamento:"SA01",
+    firma:{
+      nombre:"M.P.P Aldo Zavala García",
+      cargo:"Subdirector Administrativo"
+    },
+    asunto:"INSTALACION DEL SERVICIOS DE DIRECTORIO ACTIVO",
+    destinatarios:
+    [
+      {
+        nombre:"SUBDIRECTORES DE AREA, JEFES DE UNIDAD Y JEFES DE DEPARTAMENTO",
+        cargo:"IVEA"
+      }
+    ],
+    descripcion:"Instalación del active directoy en Dirección general",
+    elaboro:"AZG/htg",
+  },
+  {
+    documento:"Circular",
+    tipo:"emitido",
+    emision:"Interno",
+    numero:"SA/57/2015",
+    fecha:"17/11/2015",
+    nivel:"Subdirección",
+    area:"SA",
+    departamento:"SA01",
+    firma:{
+      nombre:"M.P.P Aldo Zavala García",
+      cargo:"Subdirector Administrativo"
+    },
+    asunto:"INSTALACION DEL SERVICIOS DE DIRECTORIO ACTIVO",
+    destinatarios:
+    [
+      {
+        nombre:"SUBDIRECTORES DE AREA, JEFES DE UNIDAD Y JEFES DE DEPARTAMENTO",
+        cargo:"IVEA"
+      },
+      {
+        nombre:"César Encarnación Mendoza",
+        atn:true
+      }
+    ],
+    descripcion:"Instalación del active directoy en Dirección general",
+    elaboro:"AZG/htg",
+  },
+  {
+    documento:"Circular",
+    tipo:"emitido",
+    emision:"Interno",
+    numero:"SA/57/2015",
+    fecha:"17/11/2015",
+    nivel:"Subdirección",
+    area:"SA",
+    departamento:"SA01",
+    firma:{
+      nombre:"M.P.P Aldo Zavala García",
+      cargo:"Subdirector Administrativo"
+    },
+    asunto:"INSTALACION DEL SERVICIOS DE DIRECTORIO ACTIVO",
+    destinatarios:
+    [
+      {
+        nombre:"SUBDIRECTORES DE AREA, JEFES DE UNIDAD Y JEFES DE DEPARTAMENTO",
+        cargo:"IVEA"
+      }
+    ],
+    descripcion:"Instalación del active directoy en Dirección general",
+    elaboro:"AZG/htg",
+  },
+  {
+    documento:"Circular",
+    tipo:"emitido",
+    emision:"Interno",
+    numero:"SA/57/2015",
+    fecha:"17/11/2015",
+    nivel:"Subdirección",
+    area:"SA",
+    departamento:"SA01",
+    firma:{
+      nombre:"M.P.P Aldo Zavala García",
+      cargo:"Subdirector Administrativo"
+    },
+    asunto:"INSTALACION DEL SERVICIOS DE DIRECTORIO ACTIVO",
+    destinatarios:
+    [
+      {
+        nombre:"SUBDIRECTORES DE AREA, JEFES DE UNIDAD Y JEFES DE DEPARTAMENTO",
+        cargo:"IVEA"
+      }
+    ],
+    descripcion:"Instalación del active directoy en Dirección general",
+    elaboro:"AZG/htg",
+  },
+  {
+    documento:"Circular",
+    tipo:"emitido",
+    emision:"Interno",
+    numero:"SA/57/2015",
+    fecha:"17/11/2015",
+    nivel:"Subdirección",
+    area:"SA",
+    departamento:"SA01",
+    firma:{
+      nombre:"M.P.P Aldo Zavala García",
+      cargo:"Subdirector Administrativo"
+    },
+    asunto:"INSTALACION DEL SERVICIOS DE DIRECTORIO ACTIVO",
+    destinatarios:
+    [
+      {
+        nombre:"SUBDIRECTORES DE AREA, JEFES DE UNIDAD Y JEFES DE DEPARTAMENTO",
+        cargo:"IVEA"
+      }
+    ],
+    descripcion:"Instalación del active directoy en Dirección general",
+    elaboro:"AZG/htg",
+  },
+  {
+    documento:"Circular",
+    tipo:"emitido",
+    emision:"Interno",
+    numero:"SA/57/2015",
+    fecha:"17/11/2015",
+    nivel:"Subdirección",
+    area:"SA",
+    departamento:"SA01",
+    firma:{
+      nombre:"M.P.P Aldo Zavala García",
+      cargo:"Subdirector Administrativo"
+    },
+    asunto:"INSTALACION DEL SERVICIOS DE DIRECTORIO ACTIVO",
+    destinatarios:
+    [
+      {
+        nombre:"SUBDIRECTORES DE AREA, JEFES DE UNIDAD Y JEFES DE DEPARTAMENTO",
+        cargo:"IVEA"
+      },
+      {
+        nombre:"César Encarnación Mendoza",
+        atn:true
+      }
+    ],
+    descripcion:"Instalación del active directoy en Dirección general",
+    elaboro:"AZG/htg",
+  },
+  {
+    documento:"Circular",
+    tipo:"emitido",
+    emision:"Interno",
+    numero:"SA/57/2015",
+    fecha:"17/11/2015",
+    nivel:"Subdirección",
+    area:"SA",
+    departamento:"SA01",
+    firma:{
+      nombre:"M.P.P Aldo Zavala García",
+      cargo:"Subdirector Administrativo"
+    },
+    asunto:"INSTALACION DEL SERVICIOS DE DIRECTORIO ACTIVO",
+    destinatarios:
+    [
+      {
+        nombre:"SUBDIRECTORES DE AREA, JEFES DE UNIDAD Y JEFES DE DEPARTAMENTO",
+        cargo:"IVEA"
+      }
+    ],
+    descripcion:"Instalación del active directoy en Dirección general",
+    elaboro:"AZG/htg",
+  },
+  {
+    documento:"Circular",
+    tipo:"emitido",
+    emision:"Interno",
+    numero:"SA/57/2015",
+    fecha:"17/11/2015",
+    nivel:"Subdirección",
+    area:"SA",
+    departamento:"SA01",
+    firma:{
+      nombre:"M.P.P Aldo Zavala García",
+      cargo:"Subdirector Administrativo"
+    },
+    asunto:"INSTALACION DEL SERVICIOS DE DIRECTORIO ACTIVO",
+    destinatarios:
+    [
+      {
+        nombre:"SUBDIRECTORES DE AREA, JEFES DE UNIDAD Y JEFES DE DEPARTAMENTO",
+        cargo:"IVEA"
+      }
+    ],
+    descripcion:"Instalación del active directoy en Dirección general",
+    elaboro:"AZG/htg",
+  },
+  {
+    documento:"Circular",
+    tipo:"emitido",
+    emision:"Interno",
+    numero:"SA/57/2015",
+    fecha:"17/11/2015",
+    nivel:"Subdirección",
+    area:"SA",
+    departamento:"SA01",
+    firma:{
+      nombre:"M.P.P Aldo Zavala García",
+      cargo:"Subdirector Administrativo"
+    },
+    asunto:"INSTALACION DEL SERVICIOS DE DIRECTORIO ACTIVO",
+    destinatarios:
+    [
+      {
+        nombre:"SUBDIRECTORES DE AREA, JEFES DE UNIDAD Y JEFES DE DEPARTAMENTO",
+        cargo:"IVEA"
+      }
+    ],
+    descripcion:"Instalación del active directoy en Dirección general",
+    elaboro:"AZG/htg",
   }
 ];
 
 /* GET listado de documentos emitidos. */
 router.get('/', function(req, res, next) {
-  res.render('emision/index', { title: 'Listado de documentos emitidos',documentos});
+  res.render('emision/index', { title: 'Documentos emitidos',documentos});
 });
 router.get('/registro', function(req, res, next) {
   res.render('emision/new', { title: 'Nuevo documento emitido' });
